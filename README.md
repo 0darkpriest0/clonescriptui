4dc4521a8303bda7564d5223577f3f6b51672124
Andrea <andreabernardi95@gmail.com>
2019-11-15 11:56:52 +0100

    readme.md v1.0

--- README.md
+++ README.md
@@ -25,9 +25,9 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 ```
 configuraton object
 conf:
-    j1: true/false                  -- catkin build parameter j1
-    ws_reset: true/false            -- specify if you want to reset the workspace
-    ws_init_already: true/false     -- specify if the workspace was already initialized
+    j1: true/false                          -- catkin build parameter j1
+    ws_reset: true/false                    -- specify if you want to reset the workspace
+    ws_init_already: true/false             -- specify if the workspace was already initialized
 
 array of repository object
 clone_array:
@@ -45,3 +45,22 @@ clone_array:
             "build": true/false             -- specify if you want to build this package or not
         }
     ]
+```
+
+# INIT_SCRIPT.BASH
+
+## Configuration File: clone.json
+## Directory: init_script_dir
+```
+init_script.bash generate or modify a catkin workspace named ros_catkin_ws
+It clone all the repository specified and build only the packages specified in clone.json
+```
+### Script setup
+```
+clone.json and init_script.bash must be in the same directory.
+```
+### Script launch
+```
+Workdir: init_script.bash directory
+Run command: ./init_script
+```
---
+++ init_script_dir/init_script.bash
@@ -0,0 +1,142 @@
+#!/bin/bash -       
+#title           :init_script.sh
+#description     :This script will clone clone.json's repositories and build with catkin build only the packages in whitelist
+#author		     :Andrea Bernardi   
+#usage		     :./init_script.bash
+#notes           :Install jq to use this script
+#                :need clone.json file
+#                :work in pwd/ros_catkin_ws/src
+#bash_version    :sure to work on 4.3.8 or more
+
+rm init_logfile
+touch init_logfile
+
+declare -a opt_array
+declare -a gitpath_array
+declare -a pkgsbuild_array
+declare -a pkgs_array_lenght
+
+j1=`jq '.conf.j1 ' clone.json`
+ws_reset=`jq '.conf.ws_reset' clone.json`
+ws_init_already=`jq '.conf.ws_init_already' clone.json`
+echo init configuration >> init_logfile
+echo j1-- $j1 >> init_logfile
+echo ws_reset-- $ws_reset >> init_logfile
+echo ws_init_already-- $ws_init_already >> init_logfile
+
+if [ $j1 == true ]
+then
+    opt_array[0]="-j1"
+else
+    opt_array[0]=""
+fi
+
+if [ $ws_reset == true ]
+then
+    cd ros_catkin_ws
+    catkin clean -y
+    cd ..
+    rm -rf ros_catkin_ws
+    rm -rf .catkin_tools
+    mkdir -p ros_catkin_ws/src
+    cd ros_catkin_ws/src
+    catkin_init_workspace
+    cd ..
+    cd ..
+    echo workspace reset >> init_logfile
+
+else
+    catkin clean -y
+    echo no workspace reset >> init_logfile
+
+fi
+
+if [ $ws_init_already == false ] && [ $ws_reset == false ]
+then
+    mkdir -p ros_catkin_ws/src
+    cd ros_catkin_ws/src
+    catkin_init_workspace
+    cd ..
+    cd ..
+    echo workspace init >> init_logfile
+
+else
+    echo workspace init already >> init_logfile
+
+fi
+
+value=$(jq '.clone_array[].pkgs_array | length' clone.json);
+
+for word in $value
+do
+    str+=$word";"
+done
+IFS=';' read -ra pkgs_array_lenght <<< "$str"
+
+echo finish configuration >> init_logfile
+echo init clone >> init_logfile
+echo git clone >> init_logfile
+
+for k in $(jq '.clone_array | keys | .[]' clone.json); 
+do
+    value=$(jq -r ".clone_array[$k]" clone.json);
+    clone=$(jq '.clone' <<< "$value");
+    name=$(jq -r '.name' <<< "$value");
+    repo=$(jq -r '.repository' <<< "$value");
+    branch=$(jq -r '.branch' <<< "$value");
+
+    if [ $clone == true ]
+    then
+        gitpath_array+=($repo)
+
+        echo "  -master - "$name >> init_logfile
+    
+        cd ros_catkin_ws/src
+        git clone $repo
+        cd ../..
+        
+        if [ $branch != "master" ]
+        then
+            echo "      -update from master to "$branch" - "$name >> init_logfile
+        
+            cd ros_catkin_ws/src/$name
+            git fetch --all
+            git checkout $branch
+            cd ../../..
+        else
+            echo ""
+        fi
+        
+    else
+        gitpath_array+=""
+    fi
+
+    for ((i=0; i<${pkgs_array_lenght[$k]}; i++));
+    do
+        build=$(jq -r ".clone_array[$k].pkgs_array[$i].build" clone.json);
+        
+        if [ $build == true ]
+        then
+            pkg_name=$(jq -r ".clone_array[$k].pkgs_array[$i].name" clone.json);
+            pkgsbuild_array+=" "$pkg_name;
+        else
+            pkgsbuild_array+="";
+        fi
+    done
+done
+
+echo finish clone >> init_logfile
+echo pkg to build: >> init_logfile
+
+for values in ${pkgsbuild_array[@]};
+do
+    echo "  -"$values >> init_logfile
+
+done
+
+echo start build >> init_logfile
+cd ros_catkin_ws
+catkin config --whitelist ${pkgsbuild_array[@]}
+catkin build ${opt_array[0]}
+cd ..
+echo finish build >> init_logfile
